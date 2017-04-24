175ea0bdf02605815fada2b4de4572413802d458
Andrey Seleznov <andrewaka39@gmail.com>
2017-04-24 08:18:58 +0100

    add comic style with sass

--- front_app/src/comic.jsx
+++ front_app/src/comic.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import './Comic.css';
 
 class Comic extends Component {
   render() {
     return(
       <div className="Comic">
-        <div className="Overlay">
-          
+        <div className="Thumbnail">
+          <img src={this.props.thumbnail_url} alt={this.props.title}/>
         </div>
-        <img src={this.props.thumbnail_url} />
+        <div className="Overlay">
+          <div className="Cover" />
+          <div className="Description">
+            <div className="Title">
+              {this.props.title}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
